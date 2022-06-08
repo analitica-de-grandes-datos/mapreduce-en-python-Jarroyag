c88dc2ef0d288dbbd2fd1fca93a49f9077b089d6
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-07 21:04:36 -0500

    Add files via upload

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,8 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ =="__main__":
+    for line in sys.stdin:
+        for word in line.split():
+            sys.stdout.write("{}\t1\n".format(word))
