ba4c6b9489d4ea1f24a337944f1bd2c300a7e61b
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-14 18:22:07 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -3,7 +3,9 @@
 #
 import sys
 if __name__ =="__main__":
-    for line in sys.stdin:
-        for word in line.split():
-            sys.stdout.write("{}\t1\n".format(word))
-cat /tmp/wordcount/input/credit.csv | python3 mapper.py | head            
+    tabla = [line.replace("\n", "") for line in sys.stdin]
+    tabla = [line.split(",") for line in tabla]
+    col_2 = [tabla[i][2] for i in range(len(tabla))]
+
+    for line in col_2:
+        sys.stdout.write("{}\t1\n".format(line))          
