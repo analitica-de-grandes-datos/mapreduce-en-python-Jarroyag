f2b9e3298731d647aa5696dbe900c19f1088e344
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-14 18:23:16 -0500

    Update reducer.py

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -20,5 +20,5 @@ if __name__ == '__main__':
         total = val
 sys.stdout.write("{}\t{}\n".format(curkey, total))    
 
-cat /tmp/wordcount/input/credit.csv | python3 /tmp/wordcount/mapper.py | sort | python3 /tmp/reducer.py | head
+
                 
