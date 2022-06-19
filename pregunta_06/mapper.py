490f5c7be78bf91efccbf6dc49b36482d7e7adb2
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-19 15:23:56 -0500

    Add files via upload

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+
+        column = line.split(" ")
+
+        sys.stdout.write("{}\t{}\n".format(column[0], float(column[2])))
--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+
+    curkey = None
+    vmax = 0
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+           if val > vmax: 
+             vmax= val
+           if val < vmin:
+             vmin = val  
+        else:
+            if curkey is not None:
+             sys.stdout.write("{}\t{}\t{}\n".format(curkey, vmax, vmin))
+            curkey = key
+            vmax = val
+            vmin = val
+
+sys.stdout.write("{}\t{}\t{}\n".format(curkey, vmax, vmin))
