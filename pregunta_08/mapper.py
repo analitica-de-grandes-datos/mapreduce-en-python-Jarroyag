a10875a3b5438dd16328b238b5faec428db1aca2
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-19 15:24:49 -0500

    Add files via upload

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+
+        letra = line.split(" ")[0]
+        
+        valor = line.split(" ")[2]
+        valor = float(valor)
+
+        sys.stdout.write("{}\t{}\n".format(letra, valor))
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+
+    curkey = None
+    suma = 0
+    cont = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+          suma = suma + val
+          cont = cont + 1
+        else:
+            if curkey is not None:
+
+             sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, suma/cont))
+
+            curkey = key
+            suma = val
+            cont = val
+
+sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, suma/cont))
