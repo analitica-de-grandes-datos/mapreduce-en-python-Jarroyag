52584084a96a2883bfc5e8ce177103daae221ccc
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-19 15:24:27 -0500

    Add files via upload

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+
+        letra = line.split(" ")[0]
+        data = line.split(" ")[1]
+        valor = line.split(" ")[2]
+        valor = int(valor)
+
+        sys.stdout.write("{}\t{}\t{}\n".format(letra, data, valor))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+
+    vector = []
+    
+    for line in sys.stdin:
+
+        letra, data, valor = line.split("\t")
+        valor = int(valor)
+        vector.append((letra, data, valor))
+    vector.sort(key=lambda orden: (orden[0], orden[2])) 
+
+    for line in vector:
+
+
+        sys.stdout.write("{} {} {}\n".format(line[0], line[1].strip(), line[2]))
