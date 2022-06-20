01a890e7e328550702da588916ba14c63c5e53e8
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-20 15:42:59 -0500

    Add files via upload

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -2,22 +2,22 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 import sys
+
 if __name__ == '__main__':
-
-    vector = []
+   vector = []
     
-    for line in sys.stdin:
+   for line in sys.stdin:
 
-        letra, data, valor = line.split("\t")
-        valor = int(valor)
-        vector.append((letra, data, valor))
-    vector.sort(key=lambda orden: (orden[2])) 
+     letra, data, valor = line.split("\t")
+     valor = int(valor)
+     vector.append((letra, data, valor))
+   vector.sort(key=lambda orden: (orden[2])) 
 
-    c =1
-    limit =7
+   c=1
+   limit=7
 
-    for line in vector:
+   for line in vector:
      if c < limit:
 
-          sys.stdout.write("{} {} {}\n".format(line[0], line[1].strip(), line[2]))
-          c = c +1
+          sys.stdout.write("{}  {}  {}\n".format(line[0], line[1].strip(), line[2]))
+          c = c + 1
