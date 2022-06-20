1fb8572a586e80e8d9a613d7f954f2afa205de81
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-20 17:01:42 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -3,21 +3,19 @@
 #
 import sys
 
-if __name__ == '__main__':
-   vector = []
-    
-   for line in sys.stdin:
+if __name__ == "__main__":
+    vector = []
 
-     letra, data, valor = line.split("\t")
-     valor = int(valor)
-     vector.append((letra, data, valor))
-   vector.sort(key=lambda orden: (orden[2])) 
+    for line in sys.stdin:
+        letter, date, value = line.split("\t")
+        value = int(value)
+        vector.append((letter, date, value))
+    vector.sort(key=lambda orden: (orden[2]))
 
-   c=1
-   limit=7
+    c=1
+    limit=7
 
-   for line in vector:
-      if c < limit:
-
-          sys.stdout.write("{} {} {}\n".format(line[0], line[1].strip(), line[2]))
-          c = c + 1
+    for line in vector:
+        if c < limit:
+            sys.stdout.write("{}   {}   {}\n".format(line[0], line[1].strip(), line[2]))
+            c = c + 1
