134183fd85ac1c5f440259e7ba9c2e4230cd45bb
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-20 15:42:06 -0500

    Add files via upload

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -5,10 +5,9 @@ import sys
 
 if __name__ == "__main__":
     for line in sys.stdin:
-
-        letra = line.split(" ")[0]
-        data = line.split(" ")[1]
-        valor = line.split(" ")[2]
+        letra = line.split("  ")[0]
+        data = line.split("  ")[1]
+        valor = line.split("  ")[2]
         valor = int(valor)
 
         sys.stdout.write("{}\t{}\t{}\n".format(letra, data, valor))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -7,13 +7,10 @@ if __name__ == '__main__':
     vector = []
     
     for line in sys.stdin:
-
         letra, data, valor = line.split("\t")
         valor = int(valor)
         vector.append((letra, data, valor))
     vector.sort(key=lambda orden: (orden[0], orden[2])) 
 
     for line in vector:
-
-
-        sys.stdout.write("{} {} {}\n".format(line[0], line[1].strip(), line[2]))
+      sys.stdout.write("{}  {}  {}\n".format(line[0], line[1].strip(), line[2]))
