e14384441cad68fcf5fb64b51f4d335e345d0fbb
Jarroyag <101849386+Jarroyag@users.noreply.github.com>
2022-06-20 16:23:58 -0500

    Add files via upload

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -2,21 +2,21 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 import sys
+
 if __name__ == '__main__':
+  curkey = None
+  total = 0
 
-    curkey = None
-    total = 0
-    for line in sys.stdin:
+  for line in sys.stdin:
+    key, val = line.split("\t")
+    val = int(val)
 
-        key, val = line.split("\t")
-        val = int(val)
-
-        if key == curkey:
-            total += val
-        else:
-            if curkey is not None:
-              sys.stdout.write("{}\t{}\n".format(curkey, total))
-        curkey = key
-        total = val
-    sys.stdout.write("{}\t{}\n".format(curkey, total))           
+    if key == curkey:
+      total += val
+    else:
+     if curkey is not None:
+      sys.stdout.write("{}\t{}\n".format(curkey, total))
+     curkey = key
+     total = val
+  sys.stdout.write("{}\t{}\n".format(curkey, total))           
                 
